052e1a71383090447b5ccb352af8cc2516a8ac87
Ophir Bucai <ophirbucai@gmail.com>
2024-01-14 21:24:39 +0200

    Init server + client, project structure, styles

---
+++ README.md
@@ -0,0 +1,8 @@
+# Spotifik
+
+## BY the Spotifik Team - Lee Aharon, Moria Rabayoff, Ophir Bucai
+
+Setup:
+
+1. Clone the repository
+2. Setup .env similar to .env.example with Spotify Web Client SDK credentials
---
+++ src/server/index.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+const port = 5000
+
+dotenv.config()
+
+// const spotify_client_id = process.env.SPOTIFY_CLIENT_ID
+// const spotify_client_secret = process.env.SPOTIFY_CLIENT_SECRET
+
+const app = express()
+
+app.use(cors({
+    origin: 'http://localhost:5173'
+}))
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/auth/login', (req, res) => {
+})
+
+app.get('/auth/callback', (req, res) => {
+
+})
+
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`)
+})
