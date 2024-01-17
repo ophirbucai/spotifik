a413b72599fa8ac9307663dc2c136447eb44e7cb
Ophir Bucai <ophirbucai@gmail.com>
2024-01-17 20:16:33 +0200

    Add empty components for layout (scaffolding)

---
+++ src/components/Library.jsx
@@ -0,0 +1,10 @@
+export const Library = () => {
+    return (
+        <div className="library">
+            <div>+</div>
+            <ul>
+                <li></li>
+            </ul>
+        </div>
+    )
+}
