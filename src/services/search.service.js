168bc103849c44949399aa643a16ca1014627af7
Ophir Bucai <ophirbucai@gmail.com>
2024-01-20 04:14:07 +0200

    Implement search service with dummy results

--- src/services/search.service.js
+++ src/services/search.service.js
@@ -1,26 +1,153 @@
 export const searchService = {
-    search: (searchTerm) => {
+    getTracksBySearchTerm: (searchTerm) => {
         try {
-            return dummyResults.filter(({ trackName, artistName }) => `${trackName}${artistName}`.match(new RegExp(searchTerm, 'gi')))
+            const tracks =             dummyTracks.filter(({ name, artist }) => `${name}${artist}`.match(new RegExp(searchTerm, 'gi')))
+            if (!tracks) return searchService._onError('No track results found for ' + searchTerm + '.')
+            return { tracks, status: 'success', error: null }
+        } catch (e) {
+            console.log(e)
+            return searchService._onError('Something went wrong! Please try again in a few moments.')
+        }
+    },
+    getEntityById: (type, id) => {
+        if (searchService._isInvalidType(type)) {
+            return searchService._onError('Could not complete request, type is not supported.')
+        }
+        if (!id?.trim()) {
+            return searchService._onError('Could not complete request, missing ID in hook.')
+        }
+        try {
+            let result
+            switch (type) {
+                case 'track':
+                    console.log('hey', id)
+                    result = dummyTracks.find(({ _id }) => _id === id)
+                    break
+                case 'artist':
+                    result = dummyArtists.find(({ _id }) => _id === id)
+                    break
+                case 'playlist':
+                    console.log(id)
+                    result = dummyPlaylists.find(({ _id }) => _id === id)
+                    break
+                case 'album':
+                    throw 'Not implemented yet'
+            }
+            if (!result) throw `No ${type} found`
+            return searchService._onSuccess(result)
+
+        } catch (err) {
+            return searchService._onError(err)
+        }
+    },
+    getGenres: () => {
+        try {
+            return dummyGenres
         } catch (e) {
             console.log(e)
         }
-    }
+    },
+    getGenreById: (id) => {
+        try {
+            return dummyGenres.find(({ _id }) => _id === id)
+        } catch (e) {
+            console.log(e)
+        }
+    },
+    getStationsByGenre: (genre) => {
+        // TODO: Request the server by tag and receive a list of playlists that contain that tag.
+        try {
+            return dummyStations.filter(({ genres }) => genres.includes(genre))
+        } catch (e) {
+            console.log(e)
+        }
+    },
+    getPlaylistsByGenre: (genre) => {
+        try {
+            const playlists = dummyPlaylists.filter(({ genres }) => genres.includes(genre))
+            if (!playlists) return searchService._onError('No playlists found for ' + genre + '.')
+            return { playlists, status: 'success', error: null }
+        } catch (e) {
+            console.log(e)
+            return searchService._onError('Something went wrong! Please try again in a few moments.')
+        }
+    },
+    _isInvalidType: (type) => !['track', 'album', 'artist', 'playlist'].includes(type),
+    _onError: (msg) => ({
+        status: 'error',
+        data: null,
+        error: msg
+    }),
+    _onSuccess: (data) => ({
+        status: 'success',
+        data,
+        error: null,
+    })
 }
 
-const dummyResults = [
+const dummyArtists = [
     {
-        thumbnail: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/45/Skyfall_cover.png/220px-Skyfall_cover.png',
-        trackName: 'Skyfall',
-        artistName: 'Adele',
-        trackLength: 60 * 4 + 46,
-        explicit: false
-    },
-    {
-        thumbnail: 'https://upload.wikimedia.org/wikipedia/en/7/74/Adele_-_Rolling_in_the_Deep.png',
-        trackName: 'Rolling in the Deep',
-        artistName: 'Adele',
-        trackLength: 60 * 3 + 48,
-        explicit: true
+        _id: 'adele',
+        name: 'Adele'
     }
 ]
+
+const dummyTracks = [
+    {
+        _id: 'skyfall',
+        thumbnail: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/45/Skyfall_cover.png/220px-Skyfall_cover.png',
+        name: 'Skyfall',
+        artist: dummyArtists[0].name,
+        trackLength: 60 * 4 + 46,
+        explicit: false,
+        genre: 'pop'
+    },
+    {
+        _id: 'rolling_in_the_deep',
+        thumbnail: 'https://upload.wikimedia.org/wikipedia/en/7/74/Adele_-_Rolling_in_the_Deep.png',
+        name: 'Rolling in the Deep',
+        artist: dummyArtists[0].name,
+        trackLength: 60 * 3 + 48,
+        explicit: true,
+        genre: 'pop'
+    }
+]
+
+export const dummyGenres = [{ _id: 'happy', name: 'Happy', color: 'yellow' }, { _id: 'funk', name: 'Funk', color: 'green' }]
+
+const dummyPlaylists = [
+    {
+        _id: '1',
+        name: 'Adelush Playlist',
+        genres: [
+            dummyGenres[0]._id
+        ],
+        songs: [
+            dummyTracks[1]._id,
+            dummyTracks[0]._id
+        ]
+    },
+    {
+        _id: '2',
+        name: 'Happy Moments',
+        genres: [
+            dummyGenres[0]._id
+        ],
+        songs: [
+            dummyTracks[0]._id
+        ]
+    },
+    {
+        _id: '3',
+        name: 'Funky Monks',
+        genres: [
+            dummyGenres[1]._id,
+            dummyGenres[0]._id
+        ],
+        songs: [
+            dummyTracks[0]._id,
+            dummyTracks[1]._id
+        ]
+    }
+]
+
