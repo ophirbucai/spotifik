190ff2d9e99201d83883fc8ebc3738dda61d2283
Ophir Bucai <ophirbucai@gmail.com>
2024-01-20 04:14:48 +0200

    Add get entity hook, implement Track and Playlist pages

--- src/pages/Playlist.jsx
+++ src/pages/Playlist.jsx
@@ -1,5 +1,25 @@
+import { useParams } from 'react-router-dom'
+import { useGetEntity } from '../hooks/useGetEntity.jsx'
+import { ErrorMessage } from './Track.jsx'
+import { TrackCard } from '../components/TrackCard.jsx'
+
 export default function Playlist() {
+    const { id } = useParams()
+    const { playlist, error, status } = useGetEntity('playlist', id)
+
     return (
-        <>Playlist</>
+        <>
+            {status === 'loading' && 'Loading track details...'}
+            {status === 'error' && <ErrorMessage error={error} />}
+            {status === 'success' && (
+                <div className='wrapper'>
+                    {playlist.songs.map(trackId => {
+                        return <TrackCard key={trackId} trackId={trackId} />
+                    })}
+                </div>
+            )}
+        </>
     )
 }
+
+
--- src/pages/Track.jsx
+++ src/pages/Track.jsx
@@ -1,5 +1,46 @@
+import { useGetEntity } from '../hooks/useGetEntity.jsx'
+import { Link, useParams } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
 export default function Track() {
+    const { id } = useParams()
+    const { track, error, status } = useGetEntity('track', id)
+
     return (
-        <>Details</>
+        <div className='track'>
+            {status === 'loading' && 'Loading track details...'}
+            {status === 'error' && <ErrorMessage error={error} />}
+            {status === 'success' && (
+                <div className='wrapper'>
+                    <div className="wrapper-overlay">
+                        <img className='wrapper-cover' src={track.thumbnail} alt={track.name + ' by ' + track.artist} />
+                    </div>
+                    <div className='wrapper-content'>
+                        <h1>{track.name}</h1>
+                        <h2>{track.artist}</h2>
+                        <button className='btn-primary'>Listen now :)</button>
+                    </div>
+                </div>
+            )}
+        </div>
     )
+}
+
+
+export const ErrorMessage = ({ error }) => {
+    return (
+        <div className='wrapper'>
+            <div className='wrapper-content'>
+                <h1>Sorry, there was a problem!</h1>
+                <h2>{error}</h2>
+                <div>
+                    <p>Please <Link to='/help'>reach out</Link> if this error persists, so that we can solve it.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+ErrorMessage.propTypes = {
+    error: PropTypes.string.isRequired
 }
