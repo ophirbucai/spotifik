449133d9455a45a70085a5cae3cea23212a4bfc9
Ophir Bucai <ophirbucai@gmail.com>
2024-01-20 04:16:00 +0200

    Track card in a given playlist

---
+++ src/components/TrackCard.jsx
@@ -0,0 +1,20 @@
+import { useGetEntity } from '../hooks/useGetEntity.jsx'
+import PropTypes from 'prop-types'
+
+export const TrackCard = ({ trackId }) => {
+    const { track } = useGetEntity('track', trackId)
+
+    return (
+        track ? (
+            <div className='track'>
+                <button className="btn">Play</button>
+                {track.name}
+            </div>
+        ) : null
+    )
+}
+
+
+TrackCard.propTypes = {
+    trackId: PropTypes.string.isRequired
+}
