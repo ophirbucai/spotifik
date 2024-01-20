9a8b55224937e34318eeab7f67fb18d3530fde86
Ophir Bucai <ophirbucai@gmail.com>
2024-01-20 04:07:18 +0200

    Search by navigation

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,34 +1,29 @@
-import { useEffect, useState } from 'react'
-import { BrowseGenres } from '../components/BrowseGenres.jsx'
-import { SearchResults } from '../components/SearchResults'
-import { searchService } from '../services/search.service.js'
-import { useSearchParams } from 'react-router-dom'
+import { useRef, useState } from 'react'
+import { Outlet, useNavigate, useParams, useSearchParams } from 'react-router-dom'
 
 export default function Search() {
-    const [searchParams] = useSearchParams()
-    const [searchTerm, setSearchTerm] = useState('')
-    const [results, setResults] = useState((searchParams.has('search') && searchService.search(searchParams.get('search'))) || [])
+    const { term } = useParams()
+    const timeoutId = useRef(null)
+    const navigate = useNavigate()
+    const [searchTerm, setSearchTerm] = useState(term || '')
+
     const onSearchChange = (e) => {
         setSearchTerm(e.target.value)
+        debouncedNavigate(e.target.value)
     }
 
-    useEffect(() => {
-        // TODO: Debounce 300ms after user stopped typing
-        if (searchTerm.trim()) {
-            const results = searchService.search(searchTerm)
-            setResults(results || [])
-        }
-    }, [searchTerm])
+    const debouncedNavigate = (term) => {
+        clearTimeout(timeoutId.current)
+        timeoutId.current = setTimeout(() => {
+            navigate(term ? `/search/${term}` : '/search')
+        }, 300)
+    }
+
 
     return (
         <div className='search'>
             <input className='search-input' placeholder='What do you want to listen to?' value={searchTerm} onChange={onSearchChange} />
-            {(searchTerm.trim()
-                    ? results.length
-                        ? <SearchResults results={results} />
-                        : 'no results'
-                    : <BrowseGenres />
-            )}
+            <Outlet />
         </div>
     )
 }
