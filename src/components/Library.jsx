617e440fa3a1c0f0fc21ad4b100713c40b4158af
leeaharon <144386249+leeaharon@users.noreply.github.com>
2024-01-22 20:31:01 +0200

    add icons in Library.jsx

--- src/components/Library.jsx
+++ src/components/Library.jsx
@@ -1,9 +1,18 @@
+import LibraryIcon from "../assets/icons/library.svg?react"
+import RecentsIcon from "../assets/icons/recents.svg?react"
+import SearchinlibraryIcon from "../assets/icons/searchinlibrary.svg?react"
+
+
+
 export const Library = () => {
     return (
-        <div className="library">
-            <div>Your Library</div>
-            <ul>
-                <li></li>
+        <div >
+            <ul className="navigation">
+                <li>
+                    <LibraryIcon className="library-icon" />Your Library
+                    <SearchinlibraryIcon className="searchinlibrary-icon" />search 
+                    Recents<RecentsIcon className="recents-icon" />
+                </li>
             </ul>
         </div>
     )
