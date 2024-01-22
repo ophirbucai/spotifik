a530112fd27b5943639908d3a3bead1acf167ca9
leeaharon <144386249+leeaharon@users.noreply.github.com>
2024-01-22 20:31:40 +0200

    add icons in Navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,10 +1,14 @@
 import { NavLink } from "react-router-dom"
+import LogoIcon from "../assets/icons/logo.svg?react"
 import HomeIcon from "../assets/icons/home.svg?react"
 import SearchIcon from "../assets/icons/search.svg?react"
 
 export const Navigation = () => {
     return (
         <ul className="navigation">
+            <li>
+                <NavLink to="/" ><LogoIcon className="logo-icon" /></NavLink>
+            </li>
             <li>
                 <NavLink to="/" className="home"><HomeIcon className="home-icon" />Home</NavLink>
             </li>
