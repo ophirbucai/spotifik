3669d2432376edaf1b9e39e25b16ecb2c265f7b0
Ophir Bucai <ophirbucai@gmail.com>
2024-01-25 19:52:31 +0200

    Fix user select while resizing, separate into a hook

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,30 +1,8 @@
-import { useCallback, useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
+import { useResize } from '../hooks/useResize.jsx'
 
 export const Sidebar = ({ children }) => {
-    const [width, setWidth] = useState(200)
-    const [isResizing, setIsResizing] = useState(false)
-
-    const handleMouseMove = useCallback((event) => {
-        if (!isResizing) return
-        const offset = 42
-        const maxWidth = 400
-        const minWidth = 180
-        if (event.clientX - offset > maxWidth || event.clientX - offset < minWidth) return
-        setWidth(event.clientX - offset)
-    }, [isResizing])
-
-    useEffect(() => {
-        if (!isResizing) return
-
-        document.addEventListener('mousemove', handleMouseMove)
-        document.addEventListener('mouseup', () => setIsResizing(false))
-
-        return () => {
-            document.removeEventListener('mousemove', handleMouseMove)
-        }
-    }, [isResizing, handleMouseMove])
-
+    const [width, handleMouseDown] = useResize(200)
 
     const style = { '--sidebar-width': `${width}px` }
 
@@ -34,10 +12,7 @@ export const Sidebar = ({ children }) => {
             <label
                 htmlFor='resize'
                 className={'resize-handle'}
-                onMouseDown={(e) => {
-                    e.stopPropagation()
-                    setIsResizing(true)
-                }}
+                onMouseDown={handleMouseDown}
             >
             </label>
         </nav>
