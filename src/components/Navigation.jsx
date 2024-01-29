cdab2c3a0393dbb80f1e57da72d3108d712a8409
Ophir Bucai <ophirbucai@gmail.com>
2024-01-29 21:33:19 +0200

    Lint fix

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,19 +1,19 @@
-import { NavLink } from "react-router-dom"
-import LogoIcon from "../assets/icons/logo.svg?react"
-import HomeIcon from "../assets/icons/home.svg?react"
-import SearchIcon from "../assets/icons/search.svg?react"
+import { NavLink } from 'react-router-dom'
+import LogoIcon from '../assets/icons/logo.svg?react'
+import HomeIcon from '../assets/icons/home.svg?react'
+import SearchIcon from '../assets/icons/search.svg?react'
 
 export const Navigation = () => {
     return (
-        <ul className="navigation">
+        <ul className='navigation'>
             <li>
-                <NavLink to="/" ><LogoIcon className="logo-icon" /></NavLink>
+                <NavLink to='/'><LogoIcon className='logo-icon' /></NavLink>
             </li>
             <li>
-                <NavLink to="/" className="home"><HomeIcon className="home-icon" />Home</NavLink>
+                <NavLink to='/' className='home'><HomeIcon className='home-icon' />Home</NavLink>
             </li>
             <li>
-                <NavLink to="/search"><SearchIcon className="search-icon" />Search</NavLink>
+                <NavLink to='/search'><SearchIcon className='search-icon' />Search</NavLink>
             </li>
         </ul>
     )
