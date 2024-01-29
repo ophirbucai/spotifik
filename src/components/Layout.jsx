6ee12f4d9d24da73ca7b59802c877d246110bbd1
Ophir Bucai <ophirbucai@gmail.com>
2024-01-29 21:30:03 +0200

    Lint fix

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,22 +1,22 @@
-import { Navigation } from "./Navigation.jsx"
-import { Outlet } from "react-router-dom"
-import { MainHeader } from "./MainHeader.jsx"
-import { Sidebar } from "./Sidebar.jsx"
-import { Library } from "./Library.jsx"
-import { NowPlaying } from "./NowPlaying.jsx"
+import { Navigation } from './Navigation'
+import { Outlet } from 'react-router-dom'
+import { MainHeader } from './MainHeader'
+import { Sidebar } from './Sidebar'
+import { Library } from './Library'
+import { NowPlaying } from './NowPlaying'
 
 export const Layout = () => {
     return (
-        <div className="main-layout">
+        <div className='main-layout'>
             <Sidebar>
                 <Navigation />
                 <Library />
             </Sidebar>
-            <main className="content">
+            <main className='content'>
                 <MainHeader />
                 <Outlet />
             </main>
-            <footer className="now-playing">
+            <footer className='now-playing'>
                 <NowPlaying />
             </footer>
         </div>
