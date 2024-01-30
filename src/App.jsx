d4e874cf34f4b721dbbe1edf7c7434dfad87cf7b
Ophir Bucai <ophirbucai@gmail.com>
2024-01-30 21:01:18 +0200

    Fix lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -10,17 +10,15 @@ function App() {
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Layout />}>
-                    <Route path='/' element={<Suspense />}>
-                        <Route index element={<Home />} />
-                        <Route path='/search' element={<Search />}>
-                            <Route index element={<Browse />} />
-                            <Route path='/search/:term' element={<Results />} />
-                        </Route>
-                        <Route path='/genre/:id' element={<Genre />} />
-                        <Route path='/artist/:id' element={<Artist />} />
-                        <Route path='/playlist/:id' element={<Playlist />} />
-                        <Route path='/track/:id' element={<Track />} />
+                    <Route index element={<Suspense><Home /></Suspense>} />
+                    <Route path='/search' element={<Suspense><Search /></Suspense>}>
+                        <Route index element={<Browse />} />
+                        <Route path='/search/:term' element={<Results />} />
                     </Route>
+                    <Route path='/genre/:id' element={<Suspense><Genre /></Suspense>} />
+                    <Route path='/artist/:id' element={<Suspense><Artist /></Suspense>} />
+                    <Route path='/playlist/:id' element={<Suspense><Playlist /></Suspense>} />
+                    <Route path='/track/:id' element={<Suspense><Track /></Suspense>} />
                 </Route>
                 <Route path='*' element={<NotFound />} />
             </Routes>
