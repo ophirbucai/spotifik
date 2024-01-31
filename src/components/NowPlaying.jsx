2b17515e7c6d83417cc958da3a42eee7fc7ca6a8
moria0525 <moria0525@gmail.com>
2024-02-01 01:16:37 +0200

    add actions player by react-youtube, add states

--- src/components/NowPlaying.jsx
+++ src/components/NowPlaying.jsx
@@ -1,18 +1,59 @@
-// import SwitchIcon from '../assets/icons/switch.svg'
-// import PreviousIcon from '../assets/icons/previous.svg'
-// import PauseIcon from '../assets/icons/pause.svg'
-// import NextIcon from '../assets/icons/next.svg'
-// import DisablerepeatIcon from '../assets/icons/disablerepeat.svg'
-// import NowplayingviewIcon from '../assets/icons/nowplayingview.svg'
-// import QueueIcon from '../assets/icons/queue.svg'
-// import ConnecttodeviceIcon from '../assets/icons/connecttodevice.svg'
-// import MuteIcon from '../assets/icons/mute.svg'
+import SwitchIcon from '../assets/icons/switch.svg'
+import PreviousIcon from '../assets/icons/previous.svg'
+import PauseIcon from '../assets/icons/pause.svg'
+import PlayIcon from '../assets/icons/play.svg'
+import NextIcon from '../assets/icons/next.svg'
+import DisableRepeatIcon from '../assets/icons/disableRepeat.svg'
+import NowplayingviewIcon from '../assets/icons/nowplayingview.svg'
+import QueueIcon from '../assets/icons/queue.svg'
+import ConnecttodeviceIcon from '../assets/icons/connecttodevice.svg'
+import MuteIcon from '../assets/icons/mute.svg'
 import TrackIcon from '../assets/icons/track.svg'
 import { useGetEntity } from '../hooks/useGetEntity.jsx'
+import { useEffect, useRef, useState } from 'react'
+import YouTube from 'react-youtube'
+import style from '../assets/styles/modules/youtube.module.scss'
 
 export const NowPlaying = () => {
     const { track, /*error, status*/ } = useGetEntity('track', 'skyfall')
     console.log(track)
+
+    // Function to construct the YouTube URL
+
+    //const constructYouTubeUrl = ``
+    const [songStatus, setSongStatus] = useState({ play: false, duration: null })
+    const [player, setPlayer] = useState(null)
+
+    const onReady = (e) => {
+        setPlayer(e.target)
+        const duration = e.target.getDuration()
+        setSongStatus({
+            ...songStatus, duration
+        })
+    }
+    function playSong() {
+        player.playVideo()
+        setSongStatus({
+            ...songStatus,
+            play: true
+
+        })
+    }
+    function pauseSong() {
+        player.pauseVideo()
+        setSongStatus({
+            ...songStatus,
+            play: false
+        })
+    }
+    function onDurationChange(mouseUp = false) {
+
+        return function (event) {
+            if (mouseUp)
+                player.seekTo(event.target.value, mouseUp)
+            //console.log("mouseUp", event.target.value)
+        }
+    }
 
     return (
         <div className='player'>
@@ -23,14 +64,44 @@ export const NowPlaying = () => {
                         : <TrackIcon className='track-icon' />}
                 </div>
             </div>
-            <div className='player-controls'></div>
-            <div className='player-deck'></div>
+            <div className='player-controls'>
+                <div className='player-controls-top'>
+                    <button><SwitchIcon className="switch-icon" /></button>
+                    <button onClick={player?.previousVideo}><PreviousIcon className="previous-icon" /></button>
+                    <button onClick={songStatus.play ? pauseSong : playSong}>
+                        {songStatus.play ? <PauseIcon className="pause-icon" /> : <PlayIcon className="play-icon" />}
+                    </button>
+                    <button onClick={player?.nextVideo}><NextIcon className="next-icon" /></button>
+                    <button><DisableRepeatIcon className="disablerepeat-icon" /></button>
+                </div>
+                <div className='player-controls-bottom'>
+                    <input type='range' min={0} max={songStatus.duration || 0} onChange={onDurationChange()} onMouseUp={onDurationChange(true)} />
+                </div>
+            </div>
+            <div className='player-deck'>
+                {/* {track && (<iframe allow="autoplay; encrypted-media" width={"20px"} height={"20px"} ref={playerRef}
+                    src={`https://www.youtube.com/embed/${track.youtubeId}/?origin=${import.meta.env.BASE_URL}&autoplay=1&enablejsapi=1`}></iframe>)}
+            */}
+                {track && (<YouTube
+                    videoId={track.youtubeId}
+                    // id={string}                       // defaults -> ''
+                    className={style.youtube}                // defaults -> ''
+                    // iframeClassName={string}          // defaults -> ''
 
-            {/* <button><SwitchIcon className="switch-icon" /></button>
-            <button><PreviousIcon className="previous-icon" /></button>
-            <button><PauseIcon className="pause-icon" /></button>
-            <button><NextIcon className="next-icon" /></button>
-            <button><DisablerepeatIcon className="disablerepeat-icon" /></button>
+                    // title={string}                    // defaults -> ''
+                    // loading={string}                  // defaults -> undefined
+                    // opts={obj}                        // defaults -> {}
+                    onReady={onReady}                    // defaults -> noop
+                    onPlay={playSong}                     // defaults -> noop
+                    onPause={pauseSong}                    // defaults -> noop
+                    // onEnd={func}                      // defaults -> noop
+                    // onError={func}                    // defaults -> noop
+                    onStateChange={console.log}              // defaults -> noop
+                // onPlaybackRateChange={func}       // defaults -> noop
+                // onPlaybackQualityChange={func}    // defaults -> noop
+                />)}</div>
+
+            {/* 
             <button><NowplayingviewIcon className="nowplayingview-icon" /></button>
             <button><QueueIcon className="queue-icon" /></button>
             <button><ConnecttodeviceIcon className="connecttodevice-icon" /></button>
@@ -38,3 +109,5 @@ export const NowPlaying = () => {
         </div>
     )
 }
+
+
