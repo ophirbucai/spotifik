637fb494cf6449c1fa4050ec1ec3d5873fc8019e
Ophir Bucai <ophirbucai@gmail.com>
2024-03-09 03:15:57 +0200

    Move navigation and library into sidebar, instead of through children

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,17 +1,12 @@
-import { Navigation } from './Navigation'
 import { Outlet } from 'react-router-dom'
 import { MainHeader } from './MainHeader'
 import { Sidebar } from './Sidebar'
-import { Library } from './Library'
 import { NowPlaying } from './NowPlaying'
 
 export const Layout = () => {
     return (
         <div className='main-layout'>
-            <Sidebar>
-                <Navigation />
-                <Library />
-            </Sidebar>
+            <Sidebar />
             <main className='content'>
                 <MainHeader />
                 <Outlet />
