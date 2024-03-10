677cf734fc1c86bd0024bcd13e0c826314f06504
Ophir Bucai <ophirbucai@gmail.com>
2024-03-10 19:06:24 +0200

    Move TrackBar to a separate component

--- src/components/PlayerProgress.jsx
+++ src/components/PlayerProgress.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types'
 import { useCallback, useEffect } from 'react'
+import { TrackBar } from './TrackBar.jsx'
 
 export const PlayerProgress = ({ player, songStatus, progress, setProgress }) => {
     function formatTime(time) {
@@ -23,21 +24,11 @@ export const PlayerProgress = ({ player, songStatus, progress, setProgress }) =>
         return () => window.removeEventListener('mouseup', onDurationChange)
     }, [onDurationChange, progress])
 
-    const progressBarStyle = { width: `${(progress || songStatus.currentTime) / songStatus.duration * 100}%` }
 
     return (
         <div className='player-controls-bottom'>
             <div className='progress-time-now'>{formatTime(songStatus.currentTime)}</div>
-            <div className='progress-bar'>
-                <div className='progress-bar-fill' style={progressBarStyle}></div>
-            </div>
-            <input
-                type='range'
-                min={0}
-                max={songStatus.duration || 0}
-                value={(progress || songStatus.currentTime) || 0}
-                onChange={(event) => setProgress(event.target.value)}
-            />
+            <TrackBar max={songStatus.duration || 0} value={(progress || songStatus.currentTime) || 0} onChange={(event) => setProgress(event.target.value)} />
             <div className='progress-time-end'>{formatTime(songStatus.duration)}</div>
         </div>
     )
---
+++ src/components/TrackBar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export const TrackBar = ({ max = 100, value, onChange }) => {
+    const trackBarStyle = { width: `${value / max * 100}%` }
+    return (
+        <div className='track-bar'>
+            <input
+                className='track-bar-range'
+                type='range'
+                min={0}
+                max={max}
+                value={value}
+                onChange={onChange}
+            />
+            <div className='track-bar-back'>
+                <div className='track-bar-fill' style={trackBarStyle}></div>
+            </div>
+        </div>
+    )
+}
+
+TrackBar.propTypes = {
+    max: PropTypes.number,
+    value: PropTypes.number.isRequired,
+    onChange: PropTypes.func.isRequired
+}
