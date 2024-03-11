553ec73fece2d96e67af028095b16dd219d33aaf
moria0525 <moria0525@gmail.com>
2024-03-11 21:09:38 +0200

    add button 'add to queue'

--- src/components/TrackCard.jsx
+++ src/components/TrackCard.jsx
@@ -4,13 +4,14 @@ import { useQueue } from '../store/useQueue.js'
 
 export const TrackCard = ({ trackId }) => {
     const { track } = useGetEntity('track', trackId)
-    const { addNext } = useQueue()
+    const { addNext, add } = useQueue()
 
     return (
         track ? (
             <div className='track'>
                 <button className='btn' onClick={() => addNext(trackId)}>Play</button>
                 {track.name}
+                <button className='btn' onClick={() => add(trackId)}>Add To Queue</button>
             </div>
         ) : null
     )
