296575f53de4eaa1aba638b9408cff5ab2922a40
moria0525 <moria0525@gmail.com>
2024-03-11 21:09:05 +0200

    check oueue length

--- src/components/PlayerControls.jsx
+++ src/components/PlayerControls.jsx
@@ -1,12 +1,15 @@
 import PropTypes from 'prop-types'
-import SwitchIcon from '../assets/icons/switch.svg'
-import PreviousIcon from '../assets/icons/previous.svg'
-import PauseIcon from '../assets/icons/pause.svg'
-import PlayIcon from '../assets/icons/play.svg'
-import NextIcon from '../assets/icons/next.svg'
-import RepeatIcon from '../assets/icons/repeat.svg'
+import SwitchIcon from '../assets/icons/switch.svg?react'
+import PreviousIcon from '../assets/icons/previous.svg?react'
+import PauseIcon from '../assets/icons/pause.svg?react'
+import PlayIcon from '../assets/icons/play.svg?react'
+import NextIcon from '../assets/icons/next.svg?react'
+import RepeatIcon from '../assets/icons/repeat.svg?react'
+import { useQueue } from '../store/useQueue'
+
 
 export const PlayerControls = ({ player, songStatus }) => {
+    const { remove, prev, queue, pastQueue} = useQueue()
     function onPlay() {
         player.playVideo()
     }
@@ -14,19 +17,19 @@ export const PlayerControls = ({ player, songStatus }) => {
         player.pauseVideo()
     }
     function nextSong() {
-        player.nextVideo()
+        remove()
     }
     function previousSong() {
-        player.previousVideo()
+        prev()
     }
     return (
         <div className='player-controls-top'>
             <button disabled={player === null}><SwitchIcon className='switch-icon' /></button>
-            <button disabled={player === null} onClick={previousSong}><PreviousIcon className='previous-icon' /></button>
+            <button disabled={player === null && pastQueue.length === 0} onClick={previousSong}><PreviousIcon className='previous-icon' /></button>
             <button disabled={player === null} onClick={songStatus.play ? onPause : onPlay}>
                 {songStatus.play ? <PauseIcon className='pause-icon' /> : <PlayIcon className='play-icon' />}
             </button>
-            <button disabled={player === null} onClick={nextSong}><NextIcon className='next-icon' /></button>
+            <button disabled={player === null && queue.length <= 1} onClick={nextSong}><NextIcon className='next-icon' /></button>
             <button disabled={player === null}><RepeatIcon className='repeat-icon' /></button>
         </div>
     )
