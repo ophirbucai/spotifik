260caeb550df9351f16b148c73c9d1b622e806f8
moria0525 <moria0525@gmail.com>
2024-03-11 21:10:10 +0200

    add fields useOueue

--- src/store/useQueue.js
+++ src/store/useQueue.js
@@ -1,8 +1,12 @@
+
 import { create } from 'zustand'
 
 export const useQueue = create((set) => ({
     queue: [],
+    pastQueue: [],
     add: (item) => set((state) => ({ queue: [...state.queue, item] })),
-    remove: () => set((state) => ({ queue: state.queue.slice(1) })),
+    remove: () => set((state) => ({ queue: state.queue.slice(1), pastQueue: [...state.pastQueue, state.queue[0] ] })),
+    prev: () => set((state) => ({queue: [state.pastQueue[state.pastQueue.length-1], ...state.queue], pastQueue: state.pastQueue.slice(0,-1)})),
     addNext: (item) => set((state) => ({ queue: [item, ...state.queue] }))
 }))
+
