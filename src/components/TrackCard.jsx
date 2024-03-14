feb4419dd000917e7456fe712690a6e742682ab0
moria0525 <moria0525@gmail.com>
2024-03-15 00:38:20 +0200

    add fields track card to show in playlist

--- src/components/TrackCard.jsx
+++ src/components/TrackCard.jsx
@@ -1,22 +1,44 @@
 import { useGetEntity } from '../hooks/useGetEntity.jsx'
 import PropTypes from 'prop-types'
 import { useQueue } from '../store/useQueue.js'
+import { Thumbnail } from './Thumbnail.jsx'
 
-export const TrackCard = ({ trackId }) => {
+export const TrackCard = ({ trackId, index }) => {
     const { track } = useGetEntity('track', trackId)
     const { addNext, add } = useQueue()
 
+    const formattedTrackLength = formatTime(track.trackLength)
+    console.log(formattedTrackLength)
+
+    function formatTime(seconds) {
+        const minutes = Math.floor(seconds / 60)
+        const remainingSeconds = seconds % 60
+        const formattedSeconds = remainingSeconds < 10 ? `0${remainingSeconds}` : remainingSeconds
+        return `${minutes}:${formattedSeconds}`
+    }
+    
     return (
         track ? (
             <div className='track'>
-                <button className='btn' onClick={() => addNext(trackId)}>Play</button>
-                {track.name}
-                <button className='btn' onClick={() => add(trackId)}>Add To Queue</button>
+                <div className='index'> {index}</div>
+
+                <div className='info'>
+                    <div className='thumbnail'> <Thumbnail youtubeId={track.youtubeId} /></div>
+                    {/* <button className='btn' onClick={() => addNext(trackId)}>Play</button> */}
+                    <div className='info-song'>
+                        <div className='name'>{track.name}</div>
+                        <div className='artist'>{track.artist}</div>
+                    </div>
+                </div>
+                <div className='album'> Album </div>
+                <div className='date'> Date </div>
+                <div className='length'>{formatTime(track.trackLength)} </div>
+                {/* <button className='btn' onClick={() => add(trackId)}>Add To Queue</button> */}
             </div>
         ) : null
     )
-}
 
+}
 
 TrackCard.propTypes = {
     trackId: PropTypes.string.isRequired
