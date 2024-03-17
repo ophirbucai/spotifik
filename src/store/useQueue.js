259fc82cc632006f969cf0064305da211911501a
Ophir <ophirbucai@gmail.com>
2024-03-17 22:23:19 +0200

    Allow multiple songs add to queue

--- src/store/useQueue.js
+++ src/store/useQueue.js
@@ -4,7 +4,8 @@ import { create } from 'zustand'
 export const useQueue = create((set) => ({
     queue: [],
     pastQueue: [],
-    add: (item) => set((state) => ({ queue: [...state.queue, item] })),
+    /**@param items Accepts parameters as _id: string[] */
+    add: (...items) => set((state) => ({ queue: [...state.queue, ...items] })),
     remove: () => set((state) => ({ queue: state.queue.slice(1), pastQueue: [...state.pastQueue, state.queue[0] ] })),
     prev: () => set((state) => ({queue: [state.pastQueue[state.pastQueue.length-1], ...state.queue], pastQueue: state.pastQueue.slice(0,-1)})),
     addNext: (item) => set((state) => ({ queue: [item, ...state.queue] }))
