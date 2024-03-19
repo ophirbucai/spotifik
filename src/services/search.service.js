983a7eb52a862dcce704a8cbc04ed56072cdd699
moria0525 <moria0525@gmail.com>
2024-03-20 01:07:03 +0200

    move dummyTracks to file json

--- src/services/search.service.js
+++ src/services/search.service.js
@@ -1,3 +1,5 @@
+import dummyTracksData from '../data/dummyTracks.json'
+
 export const searchService = {
     getTracksBySearchTerm: (searchTerm) => {
         try {
@@ -19,17 +21,17 @@ export const searchService = {
         try {
             let result
             switch (type) {
-            case 'track':
-                result = dummyTracks.find(({ _id }) => _id === id)
-                break
-            case 'artist':
-                result = dummyArtists.find(({ _id }) => _id === id)
-                break
-            case 'playlist':
-                result = dummyPlaylists.find(({ _id }) => _id === id)
-                break
-            case 'album':
-                throw 'Not implemented yet'
+                case 'track':
+                    result = dummyTracks.find(({ _id }) => _id === id)
+                    break
+                case 'artist':
+                    result = dummyArtists.find(({ _id }) => _id === id)
+                    break
+                case 'playlist':
+                    result = dummyPlaylists.find(({ _id }) => _id === id)
+                    break
+                case 'album':
+                    throw 'Not implemented yet'
             }
             if (!result) throw `No ${type} found`
             return searchService._onSuccess(result)
@@ -97,33 +99,30 @@ const dummyArtists = [
     }
 ]
 
-const dummyTracks = [
-    {
-        _id: 'skyfall',
-        thumbnail: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/45/Skyfall_cover.png/220px-Skyfall_cover.png',
-        name: 'Skyfall',
-        youtubeId: 'DeumyOzKqgI',
-        artist: dummyArtists[0].name,
-        trackLength: 60 * 4 + 46,
-        explicit: false,
-        genre: 'pop'
-    },
-    {
-        _id: 'rolling_in_the_deep',
-        thumbnail: 'https://upload.wikimedia.org/wikipedia/en/7/74/Adele_-_Rolling_in_the_Deep.png',
-        name: 'Rolling in the Deep',
-        artist: dummyArtists[0].name,
-        trackLength: 60 * 3 + 48,
-        explicit: true,
-        genre: 'pop'
-    }
-]
+const dummyTracks = dummyTracksData
 
-export const dummyGenres = [{ _id: 'happy', name: 'Happy', color: 'yellow' }, { _id: 'funk', name: 'Funk', color: 'green' }, {
-    _id: 'ambient',
-    name: 'Ambient',
-    color: 'blue'
-}]
+const genres = ['Rock', 'Pop', 'Jazz', 'Classical', 'Electronic',
+    'Blues',
+    'Country',
+    'Reggae',
+    'Folk',
+    'Metal',
+    'Punk',
+    'Indie',
+    'Soul',
+    'Funk',
+    'Techno',
+    'House',
+    'Dubstep',
+    'Disco']
+
+export const dummyGenres = genres.map((name) => ({
+    _id: name
+        .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
+        .join('-')
+        .toLowerCase(), name
+}))
+
 
 const dummyPlaylists = [
     {
