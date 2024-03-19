d4341d29385f71df1ccb73cd6e0431b32d72adc7
moria0525 <moria0525@gmail.com>
2024-03-20 01:04:59 +0200

    add end point by genres

--- src/server/api/search/search.routes.js
+++ src/server/api/search/search.routes.js
@@ -3,8 +3,9 @@ import { searchController } from './search.controller.js'
 
 const router = Router()
 
-router.get('/:query', searchController.getTracks)
 router.get('/genres', searchController.getGenres)
+router.get('/youtube',searchController.getTracksFromYoutube)
+router.get('/:query', searchController.getTracks)
 
 
 export { router as searchRouter }
