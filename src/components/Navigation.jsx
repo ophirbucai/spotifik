fcb4f58158cf034e24d445a930ca48b2bc64f2f2
ophirbucai <ophirbucai@gmail.com>
2024-04-01 19:46:49 +0300

    Remove logo

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -6,24 +6,24 @@ import SearchIcon from '../assets/icons/search.svg'
 export const Navigation = () => {
     return (
         <div className='navigation-wrapper'>
-            <div className='navigation-logo'>
-                <a target='_blank' rel='noopener noreferrer' href='https://github.com/ophirbucai/spotifik' className='logo'>
-                    <img
-                        src={spotifikLogo}
-                        alt='Spotifik - Great app for music listening'
-                        className='logo-icon'
-                        height='30px'
-                        width='30px'
-                    />
-                    <span style={{
-                        fontFamily: 'Avenir Next',
-                        color: 'rgb(255 255 255 / 85%)',
-                        fontWeight: 600,
-                        letterSpacing: '-0.02em',
-                        fontSize: '0.9rem'
-                    }}>Spotifik</span>
-                </a>
-            </div>
+            {/*<div className='navigation-logo'>*/}
+            {/*    <a target='_blank' rel='noopener noreferrer' href='https://github.com/ophirbucai/spotifik' className='logo'>*/}
+            {/*        <img*/}
+            {/*            src={spotifikLogo}*/}
+            {/*            alt='Spotifik - Great app for music listening'*/}
+            {/*            className='logo-icon'*/}
+            {/*            height='30px'*/}
+            {/*            width='30px'*/}
+            {/*        />*/}
+            {/*        <span style={{*/}
+            {/*            fontFamily: 'Avenir Next',*/}
+            {/*            color: 'rgb(255 255 255 / 85%)',*/}
+            {/*            fontWeight: 600,*/}
+            {/*            letterSpacing: '-0.02em',*/}
+            {/*            fontSize: '0.9rem'*/}
+            {/*        }}>Spotifik</span>*/}
+            {/*    </a>*/}
+            {/*</div>*/}
             <ul className='navigation'>
                 <li>
                     <NavLink to='/' className='home-icon'>
